94f6d806f3afa58ee6e82aa78efa78a1747d7086
rsn <1135847811@qq.com>
2018-01-28 10:28:14 +0800

    第一次提交

---
+++ app/src/main/java/com/ptteng/nursedemo/MainActivity.java
@@ -0,0 +1,19 @@
+package com.ptteng.nursedemo;
+
+import android.app.Activity;
+import android.os.Build;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        setContentView(R.layout.activity_main);
+    }
+}
